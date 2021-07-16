cf1b041776840fa60cfa3309f40c070b0e3dae73
Tsukina-mochi <Tsukina7mochi@gmail.com>
2021-07-16 18:59:03 +0900

    detach JAN pattern

--- src/encoder/jan.ts
+++ src/encoder/jan.ts
@@ -1,65 +1,12 @@
 import binaryToImageData from "./binaryToImageData";
+import pattern from '../patterns/jan/pattern.json';
 
 const calcCheckDigit = function(code: string): number {
-  const numArr = code.slice(0, code.length - 2).split('').map(v => parseInt(v));
+  const numArr = code.slice(0, code.length - 1).split('').map(v => parseInt(v));
   const even = numArr.reduce((acc, cur, i) => (i % 2 === 0 ? acc + cur : acc), 0);
   const odd  = numArr.reduce((acc, cur, i) => (i % 2 === 1 ? acc + cur : acc), 0);
 
-  return (10 - (odd * 3 + even) % 10) % 10;
-}
-
-// parity combination used for 13-digit JAN
-// O: odd parity, E: even parity
-const dataPattern = [
-  'OOOOOO',
-  'OOEOEE',
-  'OOEEOE',
-  'OOEEEO',
-  'OEOOEE',
-  'OEEOOE',
-  'OEEEOO',
-  'OEOEOE',
-  'OEOEEO',
-  'OEEOEO'
-];
-// character format
-const dataChar = {
-  leftOdd: [
-    '0001101',
-    '0011001',
-    '0010011',
-    '0111101',
-    '0100011',
-    '0110001',
-    '0101111',
-    '0111011',
-    '0110111',
-    '0001011'
-  ],
-  leftEven: [
-    '0100111',
-    '0110011',
-    '0011011',
-    '0100001',
-    '0011101',
-    '0111001',
-    '0000101',
-    '0010001',
-    '0001001',
-    '0010111'
-  ],
-  right: [
-    '1110010',
-    '1100110',
-    '1101100',
-    '1000010',
-    '1011100',
-    '1001110',
-    '1010000',
-    '1000100',
-    '1001000',
-    '1110100'
-  ]
+  return (10 - (even * 3 + odd) % 10) % 10;
 }
 
 export default function JAN(code: string): ImageData {
@@ -82,10 +29,10 @@ export default function JAN(code: string): ImageData {
 
     // left data bar
     for(let i = 0; i < 6; i++) {
-      if(dataPattern[prefix][i] == 'O') {
-        binary += dataChar.leftOdd[parseInt(left[i])];
+      if(pattern[prefix].parity[i] == 'O') {
+        binary += pattern[parseInt(left[i])]["left-odd"];
       } else {
-        binary += dataChar.leftEven[parseInt(left[i])];
+        binary += pattern[parseInt(left[i])]["left-even"];
       }
     }
 
@@ -94,7 +41,7 @@ export default function JAN(code: string): ImageData {
 
     // right data bar
     for(let i = 0; i < 6; i++) {
-      binary += dataChar.right[parseInt(right[i])];
+      binary += pattern[parseInt(right[i])]["right"];
     }
 
     // right gard bar
@@ -103,8 +50,8 @@ export default function JAN(code: string): ImageData {
     // right margin
     binary += '0000000';
   } else {
-    const left = code.slice(0, 5);
-    const right = code.slice(5);
+    const left = code.slice(0, 4);
+    const right = code.slice(4);
 
     // left margin
     binary += '0000000';
@@ -113,16 +60,16 @@ export default function JAN(code: string): ImageData {
     binary += '101';
 
     // left data bar
-    for(let i = 0; i < 6; i++) {
-      binary += dataChar.leftOdd[parseInt(left[i])];
+    for(let i = 0; i < 4; i++) {
+      binary += pattern[parseInt(left[i])]["left-odd"];
     }
 
     // center bar
     binary += '01010';
 
     // right data bar
-    for(let i = 0; i < 6; i++) {
-      binary += dataChar.right[parseInt(right[i])];
+    for(let i = 0; i < 4; i++) {
+      binary += pattern[parseInt(right[i])]["right"];
     }
 
     // right gard bar

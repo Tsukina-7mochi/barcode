a2d5ec4b33eb900be4ce0e9ce970afbd3b5d94ff
Tsukina-mochi <Tsukina7mochi@gmail.com>
2021-08-02 22:26:59 +0900

    implement reader with quagga

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,6 @@ const getRadioValue = function(name: string): string | undefined {
   return (element ? element.value : undefined);
 }
 
-// decode('00000000000101000101101001110010011001100101001110011011010101100110111001010001001010000101110010010001010000000');
-// decode('00000000000101000101101001110110111000110101100110110011010101001110100001011011001011100111001010000101010000000');
-// decode('000000000001010001000000010100110010010011011110101000110101010011101010000100010011100101010000000')
-// decode('0000000000000011100010000000000001101001000010111100100100011110101111011101010110010000110110011001011110010010011110100100100111101000011010011000010100100110100001000011001011000111010110000000000')
-// decode('00000000001101001110010110011100100010110001110001011011000010100110111101101001111001011000111010110000000000')
-decode('0000000000110100001001101110100010001110110101110110001000110100010100110000110111000101000110100011100010110110111000101000010110011000111010110000000000')
-
 window.addEventListener('load', () => {
   document.getElementById('start')?.addEventListener('click', () => {
     const code = (<HTMLInputElement>document.getElementById('code'))?.value;
@@ -37,28 +30,21 @@ window.addEventListener('load', () => {
   });
 
   document.getElementById('fileUpload')?.addEventListener('change', (e: any) => {
-    const file = e.target.files[0];
-    const url = URL.createObjectURL(file);
-    console.log(url);
+    const file: File = e.target.files[0];
+    const reader = new FileReader();
 
-    const img = new Image();
-    img.onload = () => {
+    reader.onload = () => {
+      // result must be string using FileReader.readAsDataURL
+      const url = <string> reader.result;
+
       console.log(url);
-      URL.revokeObjectURL(url);
+      console.log('result: ' + read(url));
 
-      // const w = Math.min(img.naturalWidth, 1000);
-      const w = img.naturalWidth;
-      const h = w * img.naturalHeight / img.naturalWidth;
-
-      const canvas = <HTMLCanvasElement> document.createElement('canvas');
-      canvas.width = w;
-      canvas.height = h;
-      const ctx = canvas.getContext('2d');
-      ctx?.drawImage(img, 0, 0, w, h);
-      const imgData = <ImageData> ctx?.getImageData(0, 0, w, h);
-      console.log('result: ' + read(imgData));
-      ;
+      const img = new Image();
+      img.src = url;
+      document.getElementById('output')?.appendChild(img);
     }
-    img.src = url;
+
+    reader.readAsDataURL(file);
   });
 });
--- src/reader/reader.ts
+++ src/reader/reader.ts
@@ -1,62 +1,19 @@
-import toMonochrome from "./monochromeConverter";
-import toBinary from './binarizer'
-import toGrid from './gridImage';
-import createImage from "../createImage";
-import decode from '../decoder/decoder';
 import Quagga from 'quagga';
 
-const monochromeImageToImageData = function(data: Uint8ClampedArray, width: number): ImageData {
-  const arr = new Uint8ClampedArray(data.length * 4);
-
-  for(let i = 0; i < data.length; i++) {
-    arr[i * 4 + 0] = data[i];
-    arr[i * 4 + 1] = data[i];
-    arr[i * 4 + 2] = data[i];
-    arr[i * 4 + 3] = 0xFF;
-  }
-
-  return new ImageData(arr, width);
-}
-
-export default function reader(img: ImageData): string | undefined {
-  const width = img.width;
-  const height = img.height;
-
-//   const monochrome = toMonochrome(img.data);
-//
-//   document.querySelector('div#output')?.appendChild(createImage(monochromeImageToImageData(monochrome, width)));
-//
-//   const binary = toBinary(monochrome, width, 8);
-//
-//   document.querySelector('div#output')?.appendChild(createImage(monochromeImageToImageData(binary, width)));
-
-  Quagga.decodeSingle({
-    decoder: {
-      readers: ['code_128_reader', 'ean_reader']
-    },
-    locate: true,
-    src: createImage(img).src
-  }, (result) => {
-    if(result.codeResult) {
-      console.log("result", result.codeResult.code);
-    } else {
-      console.log("not detected");
-    }
+export default function reader(imageUrl: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    Quagga.decodeSingle({
+      decoder: {
+        readers: ['code_128_reader', 'ean_reader']
+      },
+      locate: true,
+      src: imageUrl
+    }, (result) => {
+      if(result.codeResult) {
+        resolve(result.codeResult.code);
+      } else {
+        reject('NOT_DETECTED');
+      }
+    });
   });
-
-//   const y = 430;
-//   for(let gridSize = 1; gridSize < width * 0.01; gridSize += 0.1) {
-//     // grid
-//     const gridded = toGrid(binary, width, gridSize);
-//
-//     // scan
-//     const scanLine = function(y: number): string | undefined {
-//       const str = gridded.slice(y * width, (y + 1) * width).reduce((acc, cur) => acc + (cur > 0x7F ? '0' : '1'), '');
-//       return decode(str);
-//     }
-//     const result = scanLine(Math.floor(y));
-//       if(result) return result;
-//   }
-
-  return;
 }

1365dab35d8e7e226f83221e1403268e5100e7b2
Tsukina-mochi <Tsukina7mochi@gmail.com>
2021-08-18 12:33:00 +0900

    disable native barcode detector

--- src/reader/reader.ts
+++ src/reader/reader.ts
@@ -1,19 +1,19 @@
 import Quagga from 'quagga';
-import { getBarcodeDetector } from './barcodeDetecotr';
-import urlToImageData from '../misc/urlToImageData';
+// import { getBarcodeDetector } from './barcodeDetecotr';
+// import urlToImageData from '../misc/urlToImageData';
 
 export default function reader(imageUrl: string): Promise<string> {
-  const barcodeDetector = getBarcodeDetector();
-  if(barcodeDetector) {
-    return urlToImageData(imageUrl).then((imageData) => {
-      const result = barcodeDetector.detect(imageData);
-
-      if(result?.rawValue) {
-        return <string> result.rawValue;
-      }
-      throw 'NOT_DETECTED';
-    });
-  }
+//   const barcodeDetector = getBarcodeDetector();
+//   if(barcodeDetector) {
+//     return urlToImageData(imageUrl).then((imageData) => {
+//       const result = barcodeDetector.detect(imageData);
+//
+//       if(result?.rawValue) {
+//         return <string> result.rawValue;
+//       }
+//       throw 'NOT_DETECTED';
+//     });
+//   }
 
   return new Promise((resolve, reject) => {
     Quagga.decodeSingle({
